e3849adfe6c033387b68807e1f1bce65f899e579
geodevcode <geodevinstitute@gmail.com>
2025-02-14 18:25:39 +0100

    images added

--- constants/images.ts
+++ constants/images.ts
@@ -1,11 +1,17 @@
 import noResult from "@/assets/images/no-result.png";
 import onboarding from "@/assets/images/onboarding.png";
 import brandLogo from "@/assets/images/logo.png";
+import brandLogoTwo from "@/assets/images/logo-2.png";
 import path from "@/assets/images/path.png";
+import author from "@/assets/images/author.png";
+import banner from "@/assets/images/banner.png";
 
 export default {
   onboarding,
   noResult,
   brandLogo,
+  brandLogoTwo,
   path,
+  author,
+  banner,
 };
