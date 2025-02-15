7647f3676f5546da810b1d58493bd99d4b33a53f
geodevcode <geodevinstitute@gmail.com>
2025-02-15 17:45:42 +0100

    TrendingVideo updated

--- components/TrendingVideo.tsx
+++ components/TrendingVideo.tsx
@@ -1,8 +1,17 @@
-import { View, Text } from "react-native";
+import { TouchableOpacity, Image } from "react-native";
 import React from "react";
+import images from "@/constants/images";
 
-const TrendingVideo = () => {
-  return <View></View>;
+interface Props {
+  onPress?: () => void;
+}
+export const TrendingVideo = ({ onPress }: Props) => {
+  return (
+    <TouchableOpacity
+      onPress={onPress}
+      className="flex flex-col items-start w-[170px] h-[270px] relative"
+    >
+      <Image source={images.trendingVideo} className="size-full rounded-2xl" />
+    </TouchableOpacity>
+  );
 };
-
-export default TrendingVideo;
