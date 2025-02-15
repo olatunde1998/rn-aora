361f3367c46a20323772a3445c72c004be4db8ec
geodevcode <geodevinstitute@gmail.com>
2025-02-15 17:38:53 +0100

    images added

--- constants/images.ts
+++ constants/images.ts
@@ -1,17 +1,23 @@
 import noResult from "@/assets/images/no-result.png";
 import onboarding from "@/assets/images/onboarding.png";
 import brandLogo from "@/assets/images/logo.png";
-import brandLogoTwo from "@/assets/images/logo-2.png";
+import brandLogoSmall from "@/assets/images/logoSmall.png";
 import path from "@/assets/images/path.png";
 import author from "@/assets/images/author.png";
 import banner from "@/assets/images/banner.png";
+import trendingVideo from "@/assets/images/trending-video.png";
+import trash from "@/assets/images/empty.png";
+import jsMastery from "@/assets/images/js-mastery.png";
 
 export default {
   onboarding,
   noResult,
   brandLogo,
-  brandLogoTwo,
+  brandLogoSmall,
   path,
   author,
   banner,
+  trendingVideo,
+  trash,
+  jsMastery,
 };
